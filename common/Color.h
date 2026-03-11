8d187c72c5876700f53d7961a4b45b48fd3337ce
M7moUD7AmdY <imahmoudhamdi@gmail.com>
2026-03-11 21:39:33 +0200

    color module Update

--- common/Color.h
+++ common/Color.h
@@ -1,11 +1,25 @@
 #ifndef _COLOR_H_
 #define _COLOR_H_
 
+#include <string>
+#include <map>
+#include <cstdint>
+#include "../image/Image.hpp"
 
-enum class Colors{
-    RED=0xFF,
-    BLUE=0xFC,
-    GREEN=0xFB
+const std::map<std::string,sPixel> ColorTable =
+{
+    {"black",   {0,   0,   0}},
+    {"white",   {255, 255, 255}},
+    {"red",     {255, 0,   0}},
+    {"green",   {0,   255, 0}},
+    {"blue",    {0,   0,   255}},
+    {"yellow",  {255, 255, 0}},
+    {"cyan",    {0,   255, 255}},
+    {"magenta", {255, 0,   255}},
+    {"gray",    {128, 128, 128}},
+    {"grey",    {128, 128, 128}},
+    {"orange",  {255, 165, 0}},
+    {"purple",  {128, 0,   128}}
 };
 
 #endif
